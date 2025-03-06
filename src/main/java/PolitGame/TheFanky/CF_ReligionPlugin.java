3bc99ecb967451059e5920a992c6a2b5d3a5841b
Капитан Фанда <captainFandik@gmail.com>
2025-03-07 00:01:38 +0300

    Add files via upload

---
+++ src/main/java/PolitGame/TheFanky/CF_ReligionPlugin.java
@@ -0,0 +1,77 @@
+package PolitGame.TheFanky;
+
+import PolitGame.TheFanky.commands.ReligionCommand;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Map;
+
+public final class CF_ReligionPlugin extends JavaPlugin {
+    private static Map<String, String> massages;
+    private static Map<String, String> religions;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        ConfigData.createData();
+        massages = ConfigData.loadMsgList();
+        religions = ConfigData.loadReligion();
+        getCommand("religion").setExecutor(new ReligionCommand());
+
+        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) { //
+            new ReligionExpansion().register(); //
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static String getReligionFromPlayer(Player player) {
+        File playerData = ConfigData.getPlayerData();
+        YamlConfiguration ymlPut = YamlConfiguration.loadConfiguration(playerData);
+        String religion = ymlPut.getString("players." + player.getName() + ".religion");
+        return religion;
+    }
+    public static String getReligionNameFromPlayer(Player player) {
+        File playerData = ConfigData.getPlayerData();
+        YamlConfiguration ymlPut = YamlConfiguration.loadConfiguration(playerData);
+        String religion = ymlPut.getString("players." + player.getName() + ".religion");
+        return religions.get("religions." + religion + ".name");
+    }
+    public static void setReligionToPlayer(Player player, String religion) {
+        File playerData = ConfigData.getPlayerData();
+        YamlConfiguration ymlPut = YamlConfiguration.loadConfiguration(playerData);
+        ymlPut.set("players." + player.getName() + ".religion", religion);
+        ymlPut.options().copyDefaults(true);
+        try {
+            ymlPut.save(playerData);
+        } catch (Exception e) {
+            Bukkit.getConsoleSender().sendMessage("Error to save PlayerData");
+        }
+
+    }
+    public static void clearReligion(Player player) {
+        File playerData = ConfigData.getPlayerData();
+        YamlConfiguration ymlPut = YamlConfiguration.loadConfiguration(playerData);
+        ymlPut.set("players." + player.getName(), null);
+        ymlPut.options().copyDefaults(true);
+        try {
+            ymlPut.save(playerData);
+        } catch (IOException e) {
+            Bukkit.getConsoleSender().sendMessage("Error to clear Statistic");
+        }
+    }
+    public static Map<String, String> getMassages() {
+        return massages;
+    }
+
+
+}
